fdd91b1cb1ccc61a2341babf2b802a69b9defdc0
varinder <nmvarinder@gmail.com>
2023-11-22 22:32:30 +0530

    initial plain coding structue completed with refine code

---
+++ app.js
@@ -0,0 +1,17 @@
+
+let home = document.getElementById("home");
+let profileClick = document.getElementById("profileClicked");
+
+// profileClick.addEventListener("click", () => {
+//   alert("Hey Guys, Im in working position");
+// })
+
+// profileClick.addEventListener("click", visitProfile);
+// function visitProfile() {
+//   document.getElementById("about-us").innerHTML += "<div class='bgcolor'></div>";
+// }
+
+// home.addEventListener("click", visitHome);
+// function visitHome() {
+//   document.getElementById("about-us").innerHTML = "";
+// }
